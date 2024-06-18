5b510db55f071826dea787dcf3ec2170e4aa277c
Zhuoyu Ji <zhuoyu.ji@microfocus.com>
2024-06-18 14:13:22 +0000

    Sync from server repo (22a2f7683dc)

--- vclusterops/cluster_op.go
+++ vclusterops/cluster_op.go
@@ -227,7 +227,7 @@ func (op *opBase) setLogger(logger vlog.Printer) {
 func (op *opBase) parseAndCheckResponse(host, responseContent string, responseObj any) error {
 	err := util.GetJSONLogErrors(responseContent, &responseObj, op.name, op.logger)
 	if err != nil {
-		op.logger.Error(err, "fail to parse response on host, detail", "host", host, "original responseObj", responseObj)
+		op.logger.Error(err, "fail to parse response on host, detail", "host", host, "original responseContent", responseContent)
 		return err
 	}
 	op.logger.Info("JSON response", "host", host, "responseObj", responseObj)
